28b5d3e46f7c034fd367a56b54a6623240a69c46
minsterxd <minsterxd@gmail.com>
2025-11-21 18:34:49 -0300

    Add files via upload

---
+++ src/main/java/com/industrialtech/IndustrialTech.java
@@ -0,0 +1,15 @@
+package com.industrialtech;
+
+
+import net.neoforged.fml.common.Mod;
+
+
+@Mod(IndustrialTech.MODID)
+public class IndustrialTech {
+public static final String MODID = "industrialtech";
+
+
+public IndustrialTech() {
+System.out.println("[IndustrialTech] Mod cargado correctamente.");
+}
+}
